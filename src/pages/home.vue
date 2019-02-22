a5f39e43a33d68d6cf30b58ae0194ba4dfc554e1
zjj557 <1090834949@qq.com>
2019-02-22 10:55:55 +0800

    修改

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,60 +1,51 @@
 <template>
-  <div>
-    <div class="swiper-container">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-              <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550809575913&di=1640018432c158e0060466e577c6fe2f&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2F5bafa40f4bfbfbedc92f87b675f0f736aec31f80.jpg"/>
-            </div>
-            <div class="swiper-slide">
-              <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550809680312&di=d1f640b84845d00b9e508314ae1e4aa8&imgtype=0&src=http%3A%2F%2Fimage3.cnpp.cn%2Fupload2%2Fnews%2F20130921%2F160755_73400_url.jpg"/>
-            </div>
-            <div class="swiper-slide">
-              <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550809680312&di=f3c45b44f4a3feaa3de7b68a16fb1173&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F6%2F5983d2003ce92.jpg"/>
-            </div>
-        </div>
-        <!-- 如果需要分页器 -->
-        <div class="swiper-pagination"></div>
-
-        <!-- 如果需要导航按钮 -->
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
-
-        <!-- 如果需要滚动条 -->
-        <div class="swiper-scrollbar"></div>
-    </div>
+  <div class="container">
+    <Swiper v-if="slidesReal.length > 0" :autoPlay='true' :showIndicator='true' interval="2500" duration="500">
+        <Slide @click="clickMe" v-for="(item,index) in slidesReal" :key="index">
+        	<img :src="item.src"/>
+        </Slide>
+   </Swiper>
   </div>
 </template>
 <script>
-import Swiper from 'swiper'
+import { Swiper, Slide } from 'vue-swiper-component';
 export default {
-  mounted() {
-    var mySwiper = new Swiper ('.swiper-container', {
-      loop: true, // 循环模式选项
-      autoplay:true,//等同于以下设置
-
-      // 如果需要分页器
-      pagination: {
-        el: '.swiper-pagination',
-      },
-
-      // 如果需要前进后退按钮
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-
-      // 如果需要滚动条
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    })
+  data() {
+    return {
+      slidesReal: [{
+        src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550813781157&di=8e57f20add5c34d8c7ac43a03fca9869&imgtype=0&src=http%3A%2F%2Fs1.dwstatic.com%2Fgameku%2F92%2F5E%2F925e42b1a63d5519b057330575f1a9b42848.jpg'
+      },{
+        src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550813781157&di=1dfe880e104f08a4872bc1b811f9155c&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2Fc%2F5182287c2a32e.jpg'
+      }, {
+        src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1550813781157&di=f4b12f8ef24b4315c00672702e771e62&imgtype=0&src=http%3A%2F%2Fb.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fd043ad4bd11373f0d3b5c7efa40f4bfbfbed0433.jpg'
+      }]
+    }
+  },
+  components: {
+      Swiper,
+      Slide
+  },
+  methods: {
+    clickMe() {
+      console.log(111)
+    }
   }
 }
 </script>
 <style lang="less">
-.swiper-container {
-    width: 100%;
+.container{
+  width: 600px;
+  height: 300px;
+  .wh_content{
+    height: 100%;
+    .wh_slide{
+      height: 100%;
+    }
+  }
+  img{
+    width: 600px;
     height: 300px;
+  }
 }
 </style>
 
